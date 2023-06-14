aaa710d37b63e1c0ebbbcbb334044a36e9a4a7a3
rieerep <114030611+rieerep@users.noreply.github.com>
2023-06-14 12:09:51 +0200

    modified test helpers, fixed currentWeatherTest

--- rabbit-weather-project/web-api-test/TestHelpers.cs
+++ rabbit-weather-project/web-api-test/TestHelpers.cs
@@ -22,6 +22,18 @@ namespace web_api_test
 			Assert.Equal(expectedContent, await JsonSerializer.DeserializeAsync<T?>(
 				await response.Content.ReadAsStreamAsync(), _jsonSerializerOptions));
 		}
+		public static async Task AssertResponseWithSerializedContentAsync<T>(Stopwatch stopwatch,
+		HttpResponseMessage response, System.Net.HttpStatusCode expectedStatusCode,
+		T expectedContent)
+		{
+			AssertCommonResponseParts(stopwatch, response, expectedStatusCode);
+			Assert.Equal(_jsonMediaType, response.Content.Headers.ContentType?.MediaType);
+			
+			string serializedExpectedContent = JsonSerializer.Serialize(expectedContent, _jsonSerializerOptions);
+			string serializedResponse = await response.Content.ReadAsStringAsync();
+			Assert.Equal(serializedExpectedContent, serializedResponse);
+		}
+
 
 		public static void AssertCommonResponseParts(Stopwatch stopwatch,
 			HttpResponseMessage response, System.Net.HttpStatusCode expectedStatusCode)
