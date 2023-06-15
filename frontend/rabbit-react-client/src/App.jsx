36b56820486dcb29fa2597ac035c50248b40130c
Nshoan Abdlwafa <114140621+ChasAcademy-Nshoan-Abdlwafa@users.noreply.github.com>
2023-06-15 17:32:54 +0200

    Revert "Updated react client to include weather status"

--- frontend/rabbit-react-client/src/App.jsx
+++ frontend/rabbit-react-client/src/App.jsx
@@ -1,18 +1,14 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 import './App.css'
 
-  function clickMe() {
-     alert("Button clicked!");
-   }
-
 function ApiCall() {
-  const [data, setData] = React.useState([]);
+  const [data, setData] = useState([]);
 
-  React.useEffect(() => {
+  useEffect(() => {
     const fetchData = async () => {
-      const result = await axios("https://localhost:40400/healthcheck");
+      const result = await axios("http://localhost:20400/healthcheck");
       console.log(result);
       setData(result.data);
     }
@@ -20,63 +16,16 @@ function ApiCall() {
 
     //console.log(data)
   }, [])
+};
 
-  return data ? (
-    <>
-      <h3>
-        {data.status}
-        </h3>
-    </>
-  ) : (
-    <>
-      <h3>loading repsonse</h3>
-    </>
+function App() {
 
-  )
-}
-
-function ApiStockholmWeather() {
-
-  const [weather, setWeather] = React.useState([]);
-
-  React.useEffect(() => {
-    const fetchData = async () => {
-      const result = await axios("https://localhost:40400/weatherdata");
-      console.log(result);
-      setWeather(result.data);
-    }
-    fetchData()
-
-    //console.log(data)
-  }, [])
-
-  return weather ? (
-    <p>the weather in stockholm is {weather.temp} celcius</p>
-  ) : (
-    <>
-      <h1>weather data is loading</h1>
-    </>
-  )
-}
-
-function  App() {
   return (
     <>
       <div>
         <h1>Ridiculously Rabbid Rabbit Weather</h1>
-        <p>Hello and welcome to the rabbit weather site.</p>
         <ApiCall />
       </div>
-      <div>
-        <button onClick={clickMe}>
-          Button
-        </button>
-      </div>
-      <div>
-        <h3>Stockholm weather:</h3>
-        <ApiStockholmWeather />
-      </div>
-
     </>
   )
 }
