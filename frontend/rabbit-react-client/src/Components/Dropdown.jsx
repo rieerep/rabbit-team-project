d9dba3856344e3ebc06e43f1c12501ba6f533102
Rickard <114030611+rieerep@users.noreply.github.com>
2023-06-21 01:10:00 +0200

    added dropdown (#19)

---
+++ frontend/rabbit-react-client/src/Components/Dropdown.jsx
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from "react";
+
+import styled from "styled-components";
+import axios from "axios";
+
+const Dropdown = () => {
+
+    // const [city, setCity] = useState(' ');
+
+    // const allCities = [
+    //     {
+    //         city: "Stockholm"
+    //     },
+    //     {
+    //         city: "Eskilstuna"
+    //     }
+    // ]
+
+    const [data, setData] = useState({ weather: [] });
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await axios("https://localhost:40400/currentweather");
+            console.log(result);
+            setData(result.data);
+        }
+        fetchData()
+
+        //console.log(data)
+    }, [])
+
+
+    return (
+        <>
+            <label>
+                City:
+            </label>
+            <select >
+                <option value="">Select a city</option>
+                {data.weather.map(cities => (<option >{cities.name}</option>))}
+            </select>
+        </>
+    )
+}
+
+export default Dropdown;
---
+++ frontend/rabbit-react-client/src/Components/WeatherCard.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const WeatherCard = styled.div`
+display: flex;
+flex-direction: column;
+border: 2px solid red;
+top: 0, 0, 0;
+
+`
+
+export default WeatherCard
