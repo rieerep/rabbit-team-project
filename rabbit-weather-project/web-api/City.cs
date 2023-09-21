3cc6edafc1d864e41bf2cc21e44a6fc5036f9f57
Danilo Acosta <danilo.acostadote@chasacademy.se>
2023-09-21 02:48:32 +0200

    Added video bg and text-shadows

---
+++ rabbit-weather-project/web-api/City.cs
@@ -0,0 +1,11 @@
+﻿namespace web_api
+{
+    public class City
+    {
+        public string Name { get; set; }
+        public string Weather { get; set; }
+        public double Temperature { get; set; }
+        public double Wind { get; set; }
+        public bool IsFavorite { get; set; }
+    }
+}
